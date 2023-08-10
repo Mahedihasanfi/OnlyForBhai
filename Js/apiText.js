d306c60c0e6336e40b1c88c322f2e651b1139e65
Mahedi Hasan <66641541+Mahedihasanfi@users.noreply.github.com>
2023-08-11 01:20:20 +0300

    Update apiText.js

--- Js/apiText.js
+++ Js/apiText.js
@@ -1,23 +1,38 @@
 export const apiText = document.getElementById("apiText");
 
 export default async function randomApiText() {
-  apiText.innerText = "";
-  let quote = await getRandomQuote();
-  quote.split("").forEach((character) => {
-    const characterSpan = document.createElement("span");
-    characterSpan.innerText = character;
-    apiText.appendChild(characterSpan);
-  });
+  try {
+    apiText.innerText = "";
+    let quote = await getRandomQuote();
+    const characterSpans = quote.split("").map((character) => {
+      const characterSpan = document.createElement("span");
+      characterSpan.innerText = character;
+      return characterSpan;
+    });
+    apiText.append(...characterSpans);
+  } catch (error) {
+    showErrorToUser(error);
+  }
 }
 
 // Function to fetch API data
 async function getRandomQuote() {
   try {
     const response = await fetch("https://poetrydb.org/title,random/Sonnet;3");
+    if (!response.ok) {
+      throw new Error("Failed to fetch data from the API");
+    }
     const data = await response.json();
-    const text = data[0].lines.join(" "); // Parse the data array directly
+    if (data.length === 0) {
+      throw new Error("No data received from the API");
+    }
+    const text = data[0].lines.join(" ");
     return text;
   } catch (error) {
     throw error;
   }
+}
+
+function showErrorToUser(error) {
+  apiText.innerText = `Error: ${error.message}`;
 }
