17a615d463ecb1cad0ab0c89892f41da0d3cc394
Mahedi Hasan <66641541+Mahedihasanfi@users.noreply.github.com>
2023-08-11 03:21:43 +0300

    Update localStorage.js

--- Js/localStorage.js
+++ Js/localStorage.js
@@ -1,54 +1,46 @@
-import { wpmElement } from "./typedText.js";
-
 export const accuracyElement = document.getElementById("accuracy");
 
 export default function myLocalStorage() {
-  document.addEventListener("DOMContentLoaded", function () {
-    const saveButton = document.getElementById("saveButton");
-    const dataTable = document
-      .getElementById("dataTable")
-      .getElementsByTagName("tbody")[0];
+  const dataTable = document
+    .getElementById("dataTable")
+    .getElementsByTagName("tbody")[0];
 
-    saveButton.addEventListener("click", function () {
-      const timestamp = new Date().toLocaleString();
+  // Function to update the table with saved data
+  function updateTable(dataArray) {
+    // Clear existing rows
+    dataTable.innerHTML = "";
 
-      // Generate wpm and accuracy data
-      const wpmdata = `${wpmElement.innerText}, ${timestamp}`;
-      const accuracydata = `${accuracyElement.innerText}, ${timestamp}`;
+    // Add new rows to the table
+    for (const entry of dataArray) {
+      const row = document.createElement("tr");
+      const wpmdataCell = document.createElement("td");
+      const accuracydataCell = document.createElement("td");
 
-      let savedData = JSON.parse(localStorage.getItem("savedData")) || [];
-      // Add the new data to the array
-      savedData.push({ wpmdata, accuracydata });
+      wpmdataCell.textContent = entry.wpmdata;
+      accuracydataCell.textContent = entry.accuracydata;
 
-      localStorage.setItem("savedData", JSON.stringify(savedData));
+      row.appendChild(wpmdataCell);
+      row.appendChild(accuracydataCell);
 
-      // Update the table
-      updateTable(savedData);
-    });
-
-    // Function to update the table with saved data
-    function updateTable(dataArray) {
-      // Clear existing rows
-      dataTable.innerHTML = "";
-
-      // Add new rows to the table
-      for (const entry of dataArray) {
-        const row = document.createElement("tr");
-        const wpmdataCell = document.createElement("td");
-        const accuracydataCell = document.createElement("td");
-
-        wpmdataCell.textContent = entry.wpmdata;
-        accuracydataCell.textContent = entry.accuracydata;
-
-        row.appendChild(wpmdataCell);
-        row.appendChild(accuracydataCell);
-
-        dataTable.appendChild(row);
-      }
+      dataTable.appendChild(row);
     }
+  }
 
-    // Initial table update when the page loads
-    const savedData = JSON.parse(localStorage.getItem("savedData")) || [];
+  // Initial table update when the page loads
+  const savedData = JSON.parse(localStorage.getItem("savedData")) || [];
+  updateTable(savedData);
+
+  // Listen for the custom event and save data to local storage
+  document.addEventListener("timeIsUp", function (event) {
+    const { wpmdata, accuracydata } = event.detail;
+
+    let savedData = JSON.parse(localStorage.getItem("savedData")) || [];
+    // Add the new data to the array
+    savedData.push({ wpmdata, accuracydata });
+
+    localStorage.setItem("savedData", JSON.stringify(savedData));
+
+    // Update the table
     updateTable(savedData);
   });
 }
