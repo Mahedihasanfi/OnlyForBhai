8b4602c6e43a42063e0f162c67ba993404a23720
Mahedi Hasan <66641541+Mahedihasanfi@users.noreply.github.com>
2023-08-11 03:22:32 +0300

    Update timer.js

--- Js/timer.js
+++ Js/timer.js
@@ -1,11 +1,14 @@
+import { wpmElement } from "./typedText.js";
 import { typeText } from "./main.js";
+import { accuracyElement } from "./localStorage.js";
 
 const timerElement = document.getElementById("timer");
-
 let timerInterval;
+
 export default function countDown() {
   const targetTime = new Date().getTime() + 60 * 1000;
   timerInterval = setInterval(updateTimer, 1000);
+
   function updateTimer() {
     const currentTime = new Date().getTime();
     const remainingTime = targetTime - currentTime;
@@ -14,10 +17,22 @@ export default function countDown() {
     timerElement.textContent = `${minutes}:${seconds
       .toString()
       .padStart(2, "0")}`;
+
     if (remainingTime <= 0) {
       clearInterval(timerInterval);
       timerElement.textContent = "Time is up!";
       typeText.disabled = true;
+      startButton.disabled = false;
+
+      // Generate wpm and accuracy data
+      const timestamp = new Date().toLocaleString();
+      const wpmdata = `${wpmElement.innerText}, ${timestamp}`;
+      const accuracydata = `${accuracyElement.innerText}, ${timestamp}`;
+
+      // Dispatch a custom event to indicate the time is up and data should be saved
+      document.dispatchEvent(
+        new CustomEvent("timeIsUp", { detail: { wpmdata, accuracydata } })
+      );
     }
   }
 }
